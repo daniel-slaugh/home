03ca85c0608f44dab83e0638f2daa16218615bde
Daniel Slaugh <daniel.slaugh@hotmail.com>
2025-11-26 23:50:19 -0700

    Remove filler blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,6 @@
 import { glob } from 'astro/loaders'
 import { defineCollection, z } from 'astro:content'
 
-const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
-})
-
 const recipes = defineCollection({
   loader: glob({ base: './src/content/recipes', pattern: '**/*.{md,mdx}' }),
   schema: z.object({
@@ -38,4 +24,4 @@ const recipes = defineCollection({
   }),
 })
 
-export const collections = { blog, recipes }
+export const collections = { recipes }

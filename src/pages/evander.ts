a600838a940a26a088c28dc204b35898bcf01d8a
daniel-slaugh <daniel.slaugh@hotmail.com>
2026-08-03 18:36:41 -0600

    Add character sheets

---
+++ src/pages/evander.ts
@@ -0,0 +1,9 @@
+import sheet from '../Greek Campaign/evander_sheet.html?raw'
+
+export function GET() {
+  return new Response(sheet, {
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8',
+    },
+  })
+}
